f9804320d13dc44a73988b23da85652ec510ce4c
alan <alan.marcos.7e5@itb.cat>
2022-04-22 10:44:05 +0200

    en calse

--- JavaScript/DOM/excercise6/excercise6.js
+++ JavaScript/DOM/excercise6/excercise6.js
@@ -1,15 +1,10 @@
-/*
-function insertarFila() {
-    let newRow = document.createElement("tr") //creo elemento nueva fila
 
-    let firstColumn = document.createElement("td") //creo una columna y pongo su valor, luego la otra
-    firstColumn.innerHTML = "fila 3 - celda 1"
-    let secondColumn = document.createElement("td")
-    secondColumn.innerHTML = "fila 3 - celda 2"
+function editText() {
+    let cells = document.getElementsByTagName("td")
+    let listOfCells = []
+    for (const cell in cells) {
+        listOfCells.push(cell)
+    }
 
-    newRow.appendChild(firstColumn) //agrego ambas columnas a la nueva fila anteriormente creada
-    newRow.appendChild(secondColumn)
-
-    let table = document.getElementById("Taula") //obtengo la tabla existente
-    table.appendChild(newRow) //le agrego la nueva fila, que adentro contendrá las dos columnas
-}*/
+    console.log(listOfCells)
+}
