9ecd676ff0b2f90f04ee7b63a5a7c3e8b73112a3
alan <alan.marcos.7e5@itb.cat>
2022-04-22 13:09:19 +0200

    ejer6 fin

--- JavaScript/DOM/excercise6/excercise6.js
+++ JavaScript/DOM/excercise6/excercise6.js
@@ -1,10 +1,18 @@
 
-function editText() {
-    let cells = document.getElementsByTagName("td")
-    let listOfCells = []
-    for (const cell in cells) {
-        listOfCells.push(cell)
-    }
+main()
 
-    console.log(listOfCells)
+function main() {
+    var listOfCellsTag = document.getElementsByTagName("td")
+
+    for (let i=0; i<listOfCellsTag.length; i++) {
+        let myCell = listOfCellsTag[i]
+        myCell.addEventListener("click",editInnerText);
+    } 
 }
+
+function editInnerText(myEvent) {
+    console.log("desde edit")
+    console.log(myEvent.path[0].innerText)
+    myEvent.path[0].innerText = "cambiarevent"
+}
+
