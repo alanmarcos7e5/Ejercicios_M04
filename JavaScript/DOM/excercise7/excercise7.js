0fee49a6953e45a967bc31fbb147fffb7f773fa6
Alan <alan.marcos.7e5@itb.cat>
2022-04-26 22:51:13 +0200

    ex7 finished

--- JavaScript/DOM/excercise7/excercise7.js
+++ JavaScript/DOM/excercise7/excercise7.js
@@ -2,19 +2,19 @@ main()
 function main() {
     let rows = prompt("Cantidad de filas")
     let columns = prompt("Cantidad de columnas")
-    createNewRow(rows,columns)//TODO change name of function to clarify
-    //table.appendChild(creteNewRow(rows,columns))
+    createNewRows(rows,columns)
 }
 
-function createNewRow(rows, columns) {
+function createNewRows(rows, columns) {
     let table = document.getElementById("table1")
     for (let i=0; i<rows; i++){
         let newRow = document.createElement("tr")
         for (let c=0; c<columns; c++){
             let newColumn = document.createElement("td")
+            newColumn.innerText = `Row ${i}, Column ${c}`
             newRow.appendChild(newColumn)
         }
-        table.appendChild(newRow)//TODO gives an error of appendChild(null value)
+        table.appendChild(newRow)
     }
 
 }
